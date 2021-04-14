d6096297b60dc76152f165ddedbe27bf0b3f69ff
Twin Karmakharm <hi@twin.uk.com>
2021-04-14 14:30:52 +0100

    Allows django to find static files in frontend/public

--- annotation_tool/settings.py
+++ annotation_tool/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "frontend/public"),
     os.path.join(BASE_DIR, "frontend/static"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
--- annotation_tool/urls.py
+++ annotation_tool/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 
+
 from backend import views
 
 urlpatterns = [
