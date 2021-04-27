b028d0e1eeee8624ab347a343a23266395f9c5fe
davidwilby <d.wilby@sheffield.ac.uk>
2021-04-27 14:17:31 +0100

    add projects view (in vue)

---
+++ frontend/src/views/Projects.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="projects">
+      <h2>Projects</h2>
+      <b-table striped hover :items="items"></b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectList',
+  props: {},
+  data() {
+      return {
+        items: [
+          { id: 40, name: 'test project'},
+          { id: 21, name: 'test project'},
+          { id: 89, name: 'test project'},
+          { id: 38, name: 'test project' }
+        ]
+      }
+    },
+}
+</script>
+
+<style scoped>
+</style>
