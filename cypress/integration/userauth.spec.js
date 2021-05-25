a6563c1f727106b8ae2e6f9693bfbe05ed09c86d
davidwilby <d.wilby@sheffield.ac.uk>
2021-05-25 15:10:04 +0100

    add integration tests

---
+++ cypress/integration/userauth.spec.js
@@ -0,0 +1,27 @@
+describe('User Registration Test', () => {
+
+    beforeEach(()=>{
+        // Run setup if needed
+    })
+
+  it('registers a user', () => {
+      cy.visit("/register")
+
+      cy.contains('Register')
+      cy.contains('Username').type('tester')
+      cy.contains('Email').type('test@test.com')
+      cy.contains('Password').type('123456')
+      cy.contains('Confirm Password').type('123456')
+      cy.get('form').contains('Register').click()
+      cy.contains('GATE Annotation Tool')
+  })
+
+  it('tests password mismatch', () => {
+    cy.visit("/register")
+
+    cy.contains('Register')
+    cy.contains('Password').type('123456')
+    cy.contains('Confirm Password').type('789')
+    cy.contains('Password must match')
+    })
+})
