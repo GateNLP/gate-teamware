d4145d931f94078ed88a81ee364225c6994a04c1
Twin Karmakharm <hi@twin.uk.com>
2021-05-26 11:10:53 +0100

    Added name to form field

--- frontend/src/views/Project.vue
+++ frontend/src/views/Project.vue
@@ -6,7 +6,7 @@
       <b-tab title="Configuration">
         <b-form class="mt-4 mb-4">
           <b-form-group label="Name">
-            <b-form-input v-model="local_project.name"></b-form-input>
+            <b-form-input v-model="local_project.name" name="project_name"></b-form-input>
           </b-form-group>
           <b-form-row>
             <b-col>
