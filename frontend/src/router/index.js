d337760fb7255bdba9a78667dd90a20875573e43
davidwilby <d.wilby@sheffield.ac.uk>
2021-07-23 17:20:00 +0100

    move router permission controls to a single function

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -78,37 +78,22 @@ router.beforeEach((to, from, next) => {
       return;
     }
     next("/login");
-  } else {
-    next();
   }
-});
-
-router.beforeEach((to, from, next) => {
-  if (to.matched.some((record) => record.meta.guest)) {
+  else if (to.matched.some((record) => record.meta.guest)) {
     if (store.getters.isAuthenticated) {
       next();
       return;
     }
     next();
-  } else {
-    next();
-  }
-});
-
-router.beforeEach((to, from, next) => {
-  if (to.matched.some((record) => record.meta.requiresManager)) {
+  } 
+  else if (to.matched.some((record) => record.meta.requiresManager)) {
     if (store.getters.isManager) {
       next();
       return;
     }
     next("/");
-  } else {
-    next();
   }
-});
-
-router.beforeEach((to, from, next) => {
-  if (to.matched.some((record) => record.meta.requiresAdmin)) {
+  else if (to.matched.some((record) => record.meta.requiresAdmin)) {
     if (store.getters.isAdmin) {
       next();
       return;
