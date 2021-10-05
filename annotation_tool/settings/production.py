ae6edc73bc9cfdbd9e1c5bbf2213a424a1d922d3
David Wilby <d.wilby@sheffield.ac.uk>
2021-10-05 18:02:30 +0100

    log to stdout in production and staging

--- annotation_tool/settings/production.py
+++ annotation_tool/settings/production.py
@@ -1,3 +1,5 @@
+import logging
+import sys
 from .base import *
 
 # Enable csrf in production
@@ -6,3 +8,29 @@ MIDDLEWARE.append(
 )
 
 ALLOWED_HOSTS.append('annotate.gate.ac.uk')
+
+
+LOGGING = {
+   'version': 1,
+   'disable_existing_loggers': False,
+   'formatters': {
+       'verbose': {
+           'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+       },
+   },
+   'handlers': {
+       'console': {
+           'level': 'INFO',
+           'class': 'logging.StreamHandler',
+           'stream': sys.stdout,
+           'formatter': 'verbose'
+       },
+   },
+   'loggers': {
+       '': {
+           'handlers': ['console'],
+           'level': 'INFO',
+           'propagate': True,
+       },
+   },
+}
--- annotation_tool/settings/staging.py
+++ annotation_tool/settings/staging.py
@@ -6,3 +6,28 @@ MIDDLEWARE.append(
 )
 
 ALLOWED_HOSTS.append('annotate-test.gate.ac.uk')
+
+LOGGING = {
+   'version': 1,
+   'disable_existing_loggers': False,
+   'formatters': {
+       'verbose': {
+           'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+       },
+   },
+   'handlers': {
+       'console': {
+           'level': 'INFO',
+           'class': 'logging.StreamHandler',
+           'stream': sys.stdout,
+           'formatter': 'verbose'
+       },
+   },
+   'loggers': {
+       '': {
+           'handlers': ['console'],
+           'level': 'INFO',
+           'propagate': True,
+       },
+   },
+}
