a27601dceb9c0f0b61c498519c8543854ef7d9cb
Twin Karmakharm <hi@twin.uk.com>
2021-11-02 01:32:56 +0000

    Made guideline collapsible

---
+++ frontend/src/components/CollapseText.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <b-collapse v-model="collapse">
+      <slot></slot>
+    </b-collapse>
+    <a href="#" @click.prevent="collapse = !collapse">
+      <span v-if="collapse">- Hide</span><span v-else>+ Show</span>
+    </a>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "CollapseText",
+  data() {
+    return {
+      collapse: false,
+    }
+  }
+}
+</script>
+
+<style scoped>
+.collapse-text {
+  font-size: 14px;
+  line-height: 1.5;
+}
+
+.collapse-text.collapse:not(.show) {
+  display: block;
+  height: 4.5em !important;
+  overflow: hidden;
+}
+
+.collapse-text.collapsing {
+  height: 4.5em;
+}
+
+</style>
