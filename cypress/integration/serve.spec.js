ac41d341f986549efc99db675fd6b534aa7e8127
David Wilby <d.wilby@sheffield.ac.uk>
2022-05-18 11:10:15 +0100

    add frontend test for testing and training mode

--- cypress/integration/serve.spec.js
+++ cypress/integration/serve.spec.js
@@ -359,5 +359,124 @@ describe('Site serve test', () => {
 
     })
 
+    /**
+     * Tests the entire annotation cycle including logging in, creating a project, configuring a project, upload
+     * documents, add self as annotator and annotating something.
+     */
+    it('Test annotator training and test', () => {
+        // Login through JSON request
+        cy.login("admin", "testpassword")
+
+        cy.visit("/")
+
+        // Goes to project page
+        cy.contains(projectsPageStr).click()
+        cy.get("h1").should("contain", projectsPageStr)
+
+        // Create a project
+        cy.contains("Create project").click()
+        cy.get("h1").should("contain", "New project")
+
+        cy.contains("Improperly configured project").should("be.visible")
+
+        // Change project name
+        let newProjectName = "New project name";
+        cy.get("input[name='project_name']").clear().type(newProjectName)
+
+        // Change project id field
+        cy.get("input[name='project_document_id_field']").clear().type("id")
+
+        // Change project annotation config
+        cy.get("[data-cy='editor']").type("{backspace}{backspace}{del}{del}")
+        cy.fixture("project_config_sentiment.json").then((configObj) => {
+            let outStr = JSON.stringify(configObj).replace(/([\{\}])/g, '{$1}')
+            cy.get("[data-cy='editor']").type(outStr)
+        })
+
+        // Enable training and test modes
+        cy.get("label[for='project-has-training-stage']").click()
+        cy.contains("Training stage enabled").should("be.visible")
+        cy.get("label[for='project-has-testing-stage']").click()
+        cy.contains("Testing stage enabled").should("be.visible")
+        cy.contains("Save").click()
+
+        cy.contains("Can annotate after passing").should("be.visible")
+
+        // Upload documents to project
+        cy.contains("Documents & Annotations").click()
+        cy.get("[title='Upload documents'").filter(':visible').click()
+        cy.get(".modal-dialog").get('[data-cy="file-input"]').attachFile('documents_20_items.json')
+        cy.get(".modal-dialog button").contains("Upload").click()
+        cy.get(".modal-dialog button").contains("Close").click()
+
+        // Upload training documents
+        cy.contains("Training Documents").click()
+        cy.get("[title='Upload documents'").filter(':visible').click()
+        cy.get(".modal-dialog").get('[data-cy="file-input"]').attachFile('training_document.json')
+        cy.get(".modal-dialog button").contains("Upload").click()
+        cy.get(".modal-dialog button").contains("Close").click()
+
+        // Upload test documents
+        cy.contains("Testing Documents").click()
+        cy.get("[title='Upload documents'").filter(':visible').click()
+        cy.get(".modal-dialog").get('[data-cy="file-input"]').attachFile('test_document.json')
+        cy.get(".modal-dialog button").contains("Upload").click()
+        cy.get(".modal-dialog button").contains("Close").click()
+
+        // Add admin user as annotator
+        cy.contains("Annotators").click()
+        cy.get("h5").contains("Add annotator to project").parent().contains("admin").click()
+        cy.get("h5").contains("Current annotators of the project").parent().contains("admin").should("be.visible")
+        
+        cy.contains("Improperly configured project").should("not.exist")
+        
+        //Check project name changed in project list
+        cy.contains(projectsPageStr).click()
+        cy.contains("New project name")
+
+        //Check training mode functioning
+        cy.contains(annotatePageStr).click()
+        cy.contains("New project name")
+        cy.contains("Stage: Annotator Training")
+        cy.contains("Negative").click()
+        cy.contains("Incorrect").should("be.visible")
+        cy.contains("Example explanation").should("be.visible")
+        cy.contains("Positive").click()
+        cy.contains("Correct").should("be.visible")
+        cy.contains("Example explanation").should("be.visible")
+        cy.contains("Submit").click()
+
+        // Check test mode begins after training complete
+        cy.contains(annotatePageStr).click()
+        cy.contains("New project name")
+        cy.contains("Stage: Annotator Test")
+        cy.contains("Negative").click()
+        cy.contains("Incorrect").should("not.exist")
+        cy.contains("Example explanation").should("not.exist")
+        cy.contains("Positive").click()
+        cy.contains("Correct").should("not.exist")
+        cy.contains("Example explanation").should("not.exist")
+        cy.contains("Submit").click()
+
+        //Check can annotate
+        cy.contains(annotatePageStr).click()
+        cy.contains("New project name")
+        cy.contains("Negative").click()
+        cy.contains("Submit").click()
+
+        //Check annotation exists in documents list
+        cy.contains(projectsPageStr).click()
+        cy.contains("New project name").click()
+        cy.contains("Documents & Annotations").click()
+        cy.wait(500)
+        cy.get("[data-role='annotation-display-container']").first().contains(adminUsername)
+
+        //Check annotation exists in user profile
+        cy.get("a").contains(adminUsername).click()
+        cy.contains("Profile").click()
+        cy.contains(newProjectName)
+        cy.get("[data-role='annotation-display-container']").first().contains(adminUsername)
+    })
+
 
 })
