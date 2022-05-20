86e5bf4e432c50a086e6ab30ed50824325757cf0
Twin Karmakharm <hi@twin.uk.com>
2022-05-20 15:36:46 +0100

    Remove .only in test, changed .get target for user profile menu

--- cypress/integration/serve.spec.js
+++ cypress/integration/serve.spec.js
@@ -364,7 +364,7 @@ describe('Site serve test', () => {
      * Tests the entire annotation cycle including logging in, creating a project, configuring a project, upload
      * documents, add self as annotator and annotating something.
      */
-    it.only('Test annotator training and test', () => {
+    it('Test annotator training and test', () => {
         // Login through JSON request
         cy.login("admin", "testpassword")
 
@@ -473,7 +473,7 @@ describe('Site serve test', () => {
         cy.get("[data-role='annotation-display-container']").first().contains(adminUsername)
 
         //Check annotation exists in user profile
-        cy.get(".user-profile-menu").contains(adminUsername).click()
+        cy.get(".navbar").contains(adminUsername).click()
         cy.contains("Profile").click()
         cy.contains(newProjectName)
         cy.get("[data-role='annotation-display-container']").first().contains(adminUsername)
