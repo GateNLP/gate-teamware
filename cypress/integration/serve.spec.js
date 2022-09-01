64d42c2f228f77218a37fdeea6efa0b73859e031
Twin Karmakharm <hi@twin.uk.com>
2022-09-01 14:55:30 +0100

    Setting timeout instead of using wait

--- cypress/integration/serve.spec.js
+++ cypress/integration/serve.spec.js
@@ -227,15 +227,13 @@ describe('Site serve test', () => {
         cy.contains(projectsPageStr).click()
         cy.contains("New project name").click()
         cy.contains("Documents & Annotations").click()
-        cy.wait(2000)
-        cy.get("[data-role='annotation-display-container']").first().contains(adminUsername)
+        cy.get("[data-role='annotation-display-container']", {timeout: 8000}).first().contains(adminUsername)
 
         //Check annotation exists in user profile
         cy.get(".navbar").contains(adminUsername).click()
         cy.contains("My annotations").click()
         cy.contains(newProjectName).click()
-        cy.wait(2000)
-        cy.get("[data-role='annotation-display-container']").first().contains(adminUsername)
+        cy.get("[data-role='annotation-display-container']", {timeout: 8000}).first().contains(adminUsername)
     })
 
     it('Test deleting project', () => {
@@ -476,8 +474,7 @@ describe('Site serve test', () => {
         cy.contains(projectsPageStr).click()
         cy.contains("New project name").click()
         cy.contains("Documents & Annotations").click()
-        cy.wait(500)
-        cy.get("[data-role='annotation-display-container']").first().contains(adminUsername)
+        cy.get("[data-role='annotation-display-container']", {timeout: 8000}).first().contains(adminUsername)
 
         //Check annotation exists in user profile
         cy.get(".navbar").contains(adminUsername).click()
