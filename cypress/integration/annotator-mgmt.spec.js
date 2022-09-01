29e5f06d1c65d5ab974a63c6be6c62fcd468474d
davidwilby <d.wilby@sheffield.ac.uk>
2022-09-01 18:17:08 +0100

    add integration tests for annotator management view

--- cypress/integration/annotator-mgmt.spec.js
+++ cypress/integration/annotator-mgmt.spec.js
@@ -31,11 +31,71 @@ describe('Annotator Management Test', () => {
 
         // Go to annotator management tab
         cy.contains('Annotators').click()
-        cy.contains('Select all')
         
+        // Verify that annotator table is present
+        cy.get('table').contains('td', 'annotator').should('be.visible');
+        
+        // Verify that without selections, batch button actions are not active
+        cy.get('.btn-group').contains('Make annotator').should('be.disabled')
+        cy.get('.btn-group').contains('Make active').should('be.disabled')
+        cy.get('.btn-group').contains('Complete').should('be.disabled')
+        cy.get('.btn-group').contains('Reject').should('be.disabled')
 
-        // TODO reject annotator and confirm rejected
-        // TODO make annotator active and confirm
+        // Verify that to start with, no users are selected
+        cy.get('tr')
+            .siblings()
+            .contains("[aria-selected=true]")
+            .should('not.exist')
+        
+        // select all and verify all are selected
+        cy.get('.btn').contains('Select all').click()
+        cy.get('tr')
+            .siblings()
+            .contains("[aria-selected=false]")
+            .should('not.exist')
+
+        // clear selection and verify none are selected
+        cy.get('.btn').contains('Clear selected').click()
+        cy.get('tr')
+            .siblings()
+            .contains("[aria-selected=true]")
+            .should('not.exist')
+
+        // test batch action
+        cy.contains('td', 'trainer').click()
+        cy.contains('td', 'tester').click()
+        cy.get('.btn-group').contains('Make annotator').should('not.be.disabled').click()
+        cy.contains('td', 'trainer')
+            .siblings().contains('Annotating')
+        cy.contains('td', 'tester')
+            .siblings().contains('Annotating')
+
+        cy.contains('td', 'completer')
+            .siblings().contains('Complete')
+            .click()
+        cy.contains('td', 'completer')
+            .siblings().should('contain','Completed')
+
+        // reject annotator and confirm rejected
+        cy.contains('td', 'failer')  // gives you the cell 
+            .siblings()              // gives you all the other cells in the row
+            .contains('Reject')      // finds the button
+            .click()
+        cy.contains('td', 'failer').siblings().contains('Rejected')
+            
+        // make annotator active and confirm
+        cy.contains('td', 'failer')
+            .siblings()
+            .contains('Make active')
+            .click()
+        cy.contains('td', 'failer').siblings().should('contain','Waiting')
+
+        // make annotator and confirm
+        cy.contains('td', 'failer')
+            .siblings()
+            .contains('Make annotator')
+            .click()
+        cy.contains('td', 'failer').siblings().should('contain','Annotating')
         
     })
 })
