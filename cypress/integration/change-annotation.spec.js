407cd763e713f945bd6ef0755d7cdfad7b0e3ae5
Twin Karmakharm <hi@twin.uk.com>
2022-09-01 15:32:07 +0100

    Triggering CI restart

--- cypress/integration/change-annotation.spec.js
+++ cypress/integration/change-annotation.spec.js
@@ -35,7 +35,7 @@ describe('Annotation Change Test', () => {
             cy.wrap(container).contains("Change annotation").click()
             cy.wrap(container).contains("Negative").click()
             cy.wrap(container).contains("Submit").click()
-            cy.wait(1000)
+            cy.wait(1001)
             cy.wrap(container).contains("Change annotation").click()
             cy.wrap(container).contains("Neutral").click()
             cy.wrap(container).contains("Submit").click()
