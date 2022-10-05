77cf94eb0a4e1c9ba0ba26e13ba9dc1c2e0986db
davidwilby <d.wilby@sheffield.ac.uk>
2022-10-05 16:45:45 +0100

    reinstate username in use test

--- cypress/integration/userauth.spec.js
+++ cypress/integration/userauth.spec.js
@@ -27,15 +27,14 @@ describe('User Registration Test', () => {
         cy.contains('Password must match')
         })
 
-    // it('tests username in use error', () => {
-    //     //relies on above test running first to register user
-    //     cy.visit("/register")
+    it.only('tests username in use error', () => {
+        cy.visit("/register")
     
-    //     cy.contains('Username').type('tester')
-    //     cy.contains('Email').type('test@test.com')
-    //     cy.contains('Password').type('123456')
-    //     cy.contains('Confirm Password').type('123456')
-    //     cy.get('form').contains('Register').click()
-    //     cy.contains('Username already exists')
-    //     })
+        cy.contains('Username').type('annotator')
+        cy.contains('Email').type('annotator@test.com')
+        cy.contains('Password').type('123456')
+        cy.contains('Confirm Password').type('123456')
+        cy.get('form').contains('Register').click()
+        cy.contains('Username already exists')
+        })
 })
