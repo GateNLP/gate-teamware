a16b4edabb4a3ea8431f371c75000f4be7f8fe7b
davidwilby <d.wilby@sheffield.ac.uk>
2022-10-20 15:54:24 +0100

    update test

--- cypress/integration/serve.spec.js
+++ cypress/integration/serve.spec.js
@@ -36,7 +36,7 @@ describe('Site serve test', () => {
     it('About page', () => {
         cy.visit("/")
         cy.contains('About').click()
-        cy.contains('About GATE Annotate')
+        cy.contains('About GATE Teamware')
         expect(true).to.equal(true) // Example assert
         cy.contains('TEAMWARE')
     })
