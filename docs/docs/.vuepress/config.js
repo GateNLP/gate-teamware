2b7088d1c052ad60027558e0b1341b6bd03379ca
Twin Karmakharm <hi@twin.uk.com>
2023-02-06 23:14:02 +0000

    Added framework for documentation versioning

---
+++ docs/docs/.vuepress/config.js
@@ -0,0 +1,40 @@
+const versionData = require("./versions.json")
+const path = require("path");
+module.exports = context => ({
+    title: 'GATE Teamware Documentation',
+    description: 'Documentation for GATE Teamware',
+    base: versionData.base,
+    themeConfig: {
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'User Guide', link: '/userguide/' },
+          { text: 'Developer Guide', link: '/developerguide/' }
+        ],
+        sidebar: {
+            '/userguide/': [
+                "",
+                "overview",
+                "project_management",
+                "project_config",
+                "documents_annotations_management",
+                "annotators_management"
+            ],
+            '/developerguide/': [
+                '',
+                'testing',
+                'releases',
+                "api_docs",
+
+            ],
+        },
+    },
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, versionData.frontendSource)
+            }
+        }
+    },
+
+
+})
---
+++ docs/docs/.vuepress/theme/components/VersionSelector.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <b-select :options="versionOptions" v-model="currentVersion" @change="versionChangeHandler">
+  </b-select>
+</div>
+</template>
+
+<script>
+import versionData from "../../versions.json"
+export default {
+  name: "VersionSelector",
+  data(){
+    return {
+      currentVersion: versionData.base
+    }
+  },
+  computed: {
+    versionOptions(){
+      return versionData.versions
+    }
+
+  },
+  methods: {
+    versionChangeHandler(val){
+      document.location.href = val
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
