d487160caafc04a2c3523d967c7896c833b8101c
davidwilby <d.wilby@sheffield.ac.uk>
2023-02-22 17:37:48 +0000

    put privacy policy, t&c, about links in footer

--- frontend/src/components/navbar.vue
+++ frontend/src/components/navbar.vue
@@ -27,9 +27,6 @@
               <b-icon-box-arrow-up-right
                   style="position: relative; font-size: 0.8em; padding-bottom: 0.2em;"></b-icon-box-arrow-up-right>
             </b-nav-item>
-            <b-nav-item to="/about">About</b-nav-item>
-            <b-nav-item to="/privacypolicy">Privacy</b-nav-item>
-            <b-nav-item to="/terms">Terms</b-nav-item>
           </b-navbar-nav>
 
 
---
+++ frontend/src/components/pagefooter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="footer">
+    <div>
+      <b-navbar type="light" variant="light" toggleable="md" fixed="bottom">
+        <b-navbar-nav align="right" :small=true>
+          <b-nav-text>v{{ appVersion }}</b-nav-text>
+          <b-nav-item to="/privacypolicy">Privacy Policy</b-nav-item>
+          <b-nav-item to="/terms">Terms & Conditions</b-nav-item>
+          <b-nav-item to="/about">About</b-nav-item>
+        </b-navbar-nav>
+      </b-navbar>
+    </div>
+  </div>
+</template>
+<script>
+
+import {version} from '../../../package.json'
+
+export default {
+  data: () => ({
+    appVersion: version
+  }),
+}
+</script>
+
+<style>
+</style>
