97119f5ca93c7e26a9704262917058ba23f876d9
Ian Roberts <i.roberts@dcs.shef.ac.uk>
2023-03-06 00:15:51 +0000

    Better cacheing behaviour in actions now we've switched to buildx

--- build-images.sh
+++ build-images.sh
@@ -7,6 +7,6 @@ set -o allexport
 source .env
 set +o allexport
 
-docker buildx build --load -t $IMAGE_REGISTRY$MAIN_IMAGE:$IMAGE_TAG --target backend .
+docker buildx build $BUILDX_ARGS_BACKEND --load -t $IMAGE_REGISTRY$MAIN_IMAGE:$IMAGE_TAG --target backend .
 
-docker buildx build --load -t $IMAGE_REGISTRY$STATIC_IMAGE:$IMAGE_TAG --target frontend .
+docker buildx build $BUILDX_ARGS_FRONTEND --load -t $IMAGE_REGISTRY$STATIC_IMAGE:$IMAGE_TAG --target frontend .
