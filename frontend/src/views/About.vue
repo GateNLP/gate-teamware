746e21e4afa70258a5b305a5bd38120d232182da
davidwilby <d.wilby@sheffield.ac.uk>
2023-03-09 12:19:25 +0000

    add link to repo in footer and add info to about page

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -2,16 +2,16 @@
   <div class="about container">
     <h1>About GATE Teamware</h1>
 
-    <p>
     <h2>Using Teamware</h2>
+    <p>
     Teamware is developed by the <a href="https://gate.ac.uk" target="_blank">GATE team</a>, an academic research group at The University of Sheffield.
     As a result, future funding relies on evidence of the impact that the software provides.
     If you use Teamware, please let us know using the contact form at <a href = "https://gate.ac.uk/g8/contact" target="_blank">gate.ac.uk</a>.
     Please include details on grants, publications, commercial products etc. Any information that can help us to secure future funding for our work is greatly appreciated.
     </p>
 
-    <p>
     <h3>Citation</h3>
+    <p>
     For published work that has used Teamware, please cite the <a href="https://github.com/GATENLP/gate-teamware" target="_blank">project's GitHub repository</a>. One way is to include a citation such as:
     </p>
 
@@ -23,9 +23,14 @@
 
     Please use the <b>Cite this repository</b> button at the top of the <a href="https://github.com/GATENLP/gate-teamware" target="_blank">project's GitHub repository</a> to get an up to date citation.
 
-    <p>
     <h3>Version</h3>
-    This is Teamware version {{ appVersion }}.
+    <p>
+    This instance is running Teamware version {{ appVersion }}.
+    </p>
+
+    <h3>Source Code & Licensing</h3>
+    <p>
+      Teamware is free open source software available at the <a href="https://github.com/GATENLP/gate-teamware" target="_blank">project's GitHub repository</a> under the GNU Affero General Public License v3.
     </p>
 
   </div>
